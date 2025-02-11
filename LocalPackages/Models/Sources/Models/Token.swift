c084c69da3f357b081d2c495fe8145b183818b35
Martin Lukacs <4904229+lukacs-m@users.noreply.github.com>
2025-02-11 16:38:53 +0100

    feat: beginning of 2fa implementation

---
+++ LocalPackages/Models/Sources/Models/Token.swift
@@ -0,0 +1,84 @@
+//
+// Token.swift
+// Proton Authenticator - Created on 11/02/2025.
+// Copyright (c) 2025 Proton Technologies AG
+//
+// This file is part of Proton Pass.
+//
+// Proton Pass is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Proton Pass is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Proton Pass. If not, see https://www.gnu.org/licenses/.
+
+import Foundation
+
+public struct Token: Identifiable, Sendable, Hashable, Equatable, Codable {
+    public let id: String
+    public let name: String
+    public let uri: String
+    public let period: Int
+    public let note: String?
+    public let type: TotpType
+    private let precomputedHash: Int
+
+    public init(id: String = UUID().uuidString,
+                name: String,
+                uri: String,
+                period: Int,
+                type: TotpType,
+                note: String?) {
+        self.id = id
+        self.name = name
+        self.uri = uri
+        self.period = period
+        self.note = note
+        self.type = type
+        precomputedHash = Self.computeHash(id: id,
+                                           name: name,
+                                           uri: uri,
+                                           period: period,
+                                           note: note,
+                                           type: type)
+    }
+
+    public var remainingTime: TimeInterval {
+        Double(period) - Date().timeIntervalSince1970.truncatingRemainder(dividingBy: TimeInterval(period))
+    }
+}
+
+// MARK: - Hashable
+
+// swiftlint:disable function_parameter_count
+extension Token {
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(precomputedHash)
+    }
+
+    // MARK: - Static Helper for Hash Pre computation
+
+    private static func computeHash(id: String,
+                                    name: String?,
+                                    uri: String,
+                                    period: Int,
+                                    note: String?,
+                                    type: TotpType) -> Int {
+        var hasher = Hasher()
+        hasher.combine(id)
+        hasher.combine(name)
+        hasher.combine(uri)
+        hasher.combine(period)
+        hasher.combine(note)
+        hasher.combine(type)
+        return hasher.finalize()
+    }
+}
+
+// swiftlint:enable function_parameter_count
--- LocalPackages/PresentationLayer/Package.swift
+++ LocalPackages/PresentationLayer/Package.swift
@@ -13,6 +13,7 @@ var platforms: [SupportedPlatform] = [
 
 let package = Package(
     name: "PresentationLayer",
+    defaultLocalization: "en",
     platforms: platforms,
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
@@ -22,7 +23,11 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/lukacs-m/DocScanner", branch: "main")
+        .package(url: "https://github.com/lukacs-m/DocScanner", branch: "main"),
+        .package(url: "https://github.com/hmlongco/Factory", exact: "2.4.3"),
+        .package(name: "Models", path: "../Models"),
+        .package(name: "DataLayer", path: "../DataLayer"),
+
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -31,7 +36,11 @@ let package = Package(
             name: "PresentationLayer",
             dependencies: [
                 .product(name: "DocScanner", package: "DocScanner"),
-            ]),
+                .product(name: "Factory", package: "Factory"),
+                .product(name: "Models", package: "Models"),
+                .product(name: "DataLayer", package: "DataLayer")
+            ],
+            resources: [.process("Resources")]),
         .testTarget(
             name: "PresentationLayerTests",
             dependencies: ["PresentationLayer"]
