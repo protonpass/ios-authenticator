e8eef448f6a1d25507db666d3c9cd19c50b3ff68
Martin Lukacs <4904229+lukacs-m@users.noreply.github.com>
2025-03-11 11:33:34 +0100

    feat: removed unused code

--- LocalPackages/PresentationLayer/Sources/PresentationLayer/DI+Implementation/UseCaseContainer.swift
+++ LocalPackages/PresentationLayer/Sources/PresentationLayer/DI+Implementation/UseCaseContainer.swift
@@ -33,11 +33,6 @@ final class UseCaseContainer: SharedContainer, AutoRegistering {
         self { CopyTextToClipboard() }
     }
 
-//    var generateEntryUiModels: Factory<any GenerateEntryUiModelsUseCase> {
-//        self { GenerateEntryUiModels(repository: RepositoryContainer.shared.entryRepository(),
-//                                     service: ServiceContainer.shared.entryDataService()) }
-//    }
-
     var parseImageQRCodeContent: Factory<any ParseImageQRCodeContentUseCase> {
         self { ParseImageQRCodeContent() }
     }
