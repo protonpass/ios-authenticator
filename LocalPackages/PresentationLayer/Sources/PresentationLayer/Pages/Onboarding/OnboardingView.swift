740e0049a81c78e23a1f9755c05f96d4fef7a56c
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2025-03-19 21:37:46 +0100

    feat(onboarding): empty onboard steps

---
+++ LocalPackages/PresentationLayer/Sources/PresentationLayer/Pages/Onboarding/OnboardingView.swift
@@ -0,0 +1,50 @@
+//
+// OnboardingView.swift
+// Proton Authenticator - Created on 19/03/2025.
+// Copyright (c) 2025 Proton Technologies AG
+//
+// This file is part of Proton Authenticator.
+//
+// Proton Authenticator is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Proton Authenticator is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Proton Authenticator. If not, see https://www.gnu.org/licenses/.
+
+import SwiftUI
+
+public struct OnboardingView: View {
+    @State private var viewModel = OnboardingViewModel()
+
+    let onFinish: () -> Void
+
+    public init(onFinish: @escaping () -> Void) {
+        self.onFinish = onFinish
+    }
+
+    public var body: some View {
+        switch viewModel.currentStep {
+        case .intro:
+            OnboardIntro(onNext: goNext)
+        case .import:
+            OnboardImport(onNext: goNext)
+        case .biometric:
+            OnboardBiometric(onNext: goNext)
+        }
+    }
+}
+
+private extension OnboardingView {
+    func goNext() {
+        if !viewModel.goNext() {
+            onFinish()
+        }
+    }
+}
---
+++ LocalPackages/PresentationLayer/Sources/PresentationLayer/Pages/Onboarding/OnboardingViewModel.swift
@@ -0,0 +1,45 @@
+//
+// OnboardingViewModel.swift
+// Proton Authenticator - Created on 19/03/2025.
+// Copyright (c) 2025 Proton Technologies AG
+//
+// This file is part of Proton Authenticator.
+//
+// Proton Authenticator is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Proton Authenticator is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Proton Authenticator. If not, see https://www.gnu.org/licenses/.
+
+import Foundation
+
+enum OnboardStep: Sendable {
+    case intro, `import`, biometric
+}
+
+@Observable
+final class OnboardingViewModel {
+    private(set) var currentStep: OnboardStep = .intro
+
+    init() {}
+
+    /// Return `true` if next step is available, `false` otherwise
+    func goNext() -> Bool {
+        switch currentStep {
+        case .intro:
+            currentStep = .import
+        case .import:
+            currentStep = .biometric
+        case .biometric:
+            return false
+        }
+        return true
+    }
+}
