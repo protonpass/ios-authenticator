7a2dd249bca6b3a0a7aac85e2feb5fb3143269ce
Martin Lukacs <4904229+lukacs-m@users.noreply.github.com>
2025-06-18 10:15:45 +0200

    fix: added missing module

--- LocalPackages/PresentationLayer/Sources/PresentationLayer/Shared/ViewModifiers/Import+ViewModifier.swift
+++ LocalPackages/PresentationLayer/Sources/PresentationLayer/Shared/ViewModifiers/Import+ViewModifier.swift
@@ -378,7 +378,7 @@ final class ImportViewModel {
             showCompletion(numberOfImportedEntries)
             if !errors.isEmpty {
                 alertService
-                    .showError(#localized("Several images could not be processed. See logs for more details."),
+                    .showError(#localized("Several images could not be processed. See logs for more details.", bundle: .module),
                                mainDisplay: mainDisplay,
                                action: nil)
             }
