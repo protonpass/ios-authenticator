74981fd3d159bc22effeba669c7032f45c134017
Martin Lukacs <4904229+lukacs-m@users.noreply.github.com>
2025-06-23 11:06:18 +0200

    fix: ignore module

--- LocalPackages/PresentationLayer/Sources/PresentationLayer/Shared/ViewModifiers/Import+ViewModifier.swift
+++ LocalPackages/PresentationLayer/Sources/PresentationLayer/Shared/ViewModifiers/Import+ViewModifier.swift
@@ -135,7 +135,7 @@ struct ImportingServiceModifier: ViewModifier {
                 .frame(height: 170)
                 .padding(.horizontal, 32)
 
-                Text(#localized("Import codes from", bundle: .module) + " " + option.title)
+                Text(#localized("Import codes from", bundle: .module) + " " + option.title) // ignore:missing_bundle
                     .font(.title)
                     .fontWeight(.bold)
                     .foregroundStyle(.textNorm)
