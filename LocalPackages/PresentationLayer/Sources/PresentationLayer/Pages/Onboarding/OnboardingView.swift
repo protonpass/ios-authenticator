612e7709baf6e0b751190f1d0d97ff5161975c8f
Thanh-Nhon Nguyen <thanh-nhon.nguyen@proton.ch>
2025-07-02 21:12:49 +0200

    feat: animate onboarding steps

--- LocalPackages/PresentationLayer/Sources/PresentationLayer/Pages/Onboarding/OnboardingView.swift
+++ LocalPackages/PresentationLayer/Sources/PresentationLayer/Pages/Onboarding/OnboardingView.swift
@@ -18,6 +18,7 @@
 // You should have received a copy of the GNU General Public License
 // along with Proton Authenticator. If not, see https://www.gnu.org/licenses/.
 
+import Lottie
 import Models
 import SwiftUI
 
@@ -58,20 +59,16 @@ private extension OnboardingView {
                         .frame(height: 30)
 
                 case .import:
-                    Image("locktree", bundle: .module)
-                        .resizable()
-                        .scaledToFit()
+                    LottieView(animation: .named("LockTree", bundle: .module))
+                        .playing(loopMode: .playOnce)
                         .frame(maxWidth: 185)
                     Spacer()
                         .frame(height: 60)
 
                 case .biometric:
-                    Image("FaceID", bundle: .module)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(maxWidth: 160)
-                    Spacer()
-                        .frame(height: 60)
+                    LottieView(animation: .named("FaceID", bundle: .module))
+                        .playing(loopMode: .playOnce)
+                        .frame(maxWidth: 260)
 
                 case .iCloudSync:
                     Image("iCloudSync", bundle: .module)
